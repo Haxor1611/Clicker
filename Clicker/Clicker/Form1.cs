b672ce7ccde32eab4646cbd243c2bde8c2285d83
Haxor1611 <71041042+Haxor1611@users.noreply.github.com>
2022-01-23 16:21:49 +0000

    Initial commit

---
+++ Clicker/Clicker/Form1.cs
@@ -0,0 +1,20 @@
+namespace Clicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}7
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Clicker
- Minecraft Autoclicker
+ 
